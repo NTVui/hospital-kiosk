bb8233a8a99f33ec883e0dd821bae4857bc4303a
Vui Nguyen <darkdaddu@gmail.com>
2025-08-13 17:05:25 +0700

    Thêm pagination

--- controllers/v1/admin/clinic-controller.js
+++ controllers/v1/admin/clinic-controller.js
@@ -16,7 +16,33 @@ module.exports.index = async (req,res) =>{
   //Đoạn bộ lọc
   const filterStatus = filterStatusHelper(req.query)
 
+  //Sort
+  //Quy ước: Khi FE truyền cho BE thì phải truyền thêm query nữa
+  //?sortKey=tenPhongKham&sortValue=asc
+  let sort = {}
+  if(req.query.sortKey && req.query.sortValue){
+    sort[req.query.sortKey] = req.query.sortValue
+  }else{
+    sort.position = "desc"
+  }
+  //End Sort
+
+  //Pagination
+  const countClinics = await Clinic.countDocuments(find)
+  let objectPagination = paginationHelper(
+    {
+      currentPage: 1,
+      limitItems: 4,
+    },
+    req.query,
+    countClinics,
+  )
+  //End Pagination
+
   const clinic = await Clinic.find(find)
+  .sort(sort)
+  .limit(objectPagination.limitItems)
+  .skip(objectPagination.skip)
   res.json(clinic)
   //   res.render('admin/pages/clinics/index', {
   //   pageTitle: 'Trang quản lí phòng khám',
