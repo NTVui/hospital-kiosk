04061dd7d8b72b44b29894bc28b2aad49883435c
Vui Nguyen <darkdaddu@gmail.com>
2025-08-14 12:38:18 +0700

    Thêm option changeMulti de test API, tam thoi da biet cach test => lam chinh thuc thoi!

--- controllers/v1/admin/clinic-controller.js
+++ controllers/v1/admin/clinic-controller.js
@@ -55,3 +55,89 @@ module.exports.index = async (req,res) =>{
     
   // })
 }
+
+// [GET] /admin/clinics/detail/:id
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      deleted: false,
+      _id: req.params.id,
+    }
+    const clinic = await Clinic.findOne(find)
+    
+    res.json(clinic)
+    // res.render('admin/pages/products/detail', {
+    //   pageTitle: product.title,
+    //   product: product,
+    // })
+  } catch (error) {
+    //res.redirect(`${version}${systemConfig.prefixAdmin}/clinics`)
+  }
+}
+
+// [GET] /admin/clinics/change-status/:id
+module.exports.changeStatus = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const status = req.body.status;
+    await Clinic.updateOne({
+      _id: id
+    },{
+      status: status
+    })
+    
+    res.json({
+      code: 200,
+      message: "Cập nhật trạng thái thành công"
+    })
+    } catch (error) {
+      res.json({
+      code: 400,
+      message: "Không tồn tại"
+    })
+    }
+
+}
+
+// [PATCH] /admin/clinics/change-multi/
+/*
+{
+  "type": "status",
+  "ids": "id_1,id_2,id_3",
+  "value": "Hoạt động"
+}
+*/
+module.exports.changeMulti = async (req, res) => {
+  
+  const type = req.body.type;
+  const idsString = req.body.ids;
+  const value = req.body.value; 
+
+  const ids = idsString
+    .split(',')
+    .map((id) => id.trim())
+    .filter((id) => id.length > 0);
+
+  switch (type) {
+    case 'status':
+      await Clinic.updateMany({ _id: { $in: ids } }, {
+        status: value 
+      });
+      res.json({
+        code: 200,
+        message: "Cập nhật trạng thái thành công"
+      });
+      break; 
+
+    case 'delete': // Ví dụ cho trường hợp khác
+      // ...
+      break;
+      
+    default:
+      res.json({
+        code: 400,
+        message: "Hành động không hợp lệ"
+      });
+      break;
+  }
+};
